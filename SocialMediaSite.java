5b435935980972a1b66a3f240a5e083c962f39a0
Alok Kumar <36418056+alokkiitd1729@users.noreply.github.com>
2019-01-26 04:42:49 +0530

    Update SocialMediaSite.java

--- SocialMediaSite.java
+++ SocialMediaSite.java
@@ -1,3 +1,15 @@
+
+
+/**
+ * Project completion Date 18 November 2018
+ *    Author@_Alok_Kumar
+ *    Indian Institute of Technology(IIT) Delhi
+ *
+ *    To Understand the project in enough detail please go to ReadMe file of Project1
+ *     and Visit under the section under the name of this class.
+ */
+
+
 public class SocialMediaSite {
 	MyHashTable<String,User> setofallusers = new MyHashTable<String,User>();// here string is UID
 	MyHashTable<String,Message> setofallmessages = new MyHashTable<String,Message>(); // here string is TID	
