04b51b5b2d0a735e57b5ae6ca1acc82e502f2fec
Alok Kumar <36418056+alokkiitd1729@users.noreply.github.com>
2019-01-26 04:39:29 +0530

    Update Message.java

--- Message.java
+++ Message.java
@@ -1,3 +1,15 @@
+
+
+/**
+ * Project completion Date 18 November 2018
+ *    Author@_Alok_Kumar
+ *    Indian Institute of Technology(IIT) Delhi
+ *
+ *    To Understand the project in enough detail please go to ReadMe file of Project1
+ *     and Visit under the section under the name of this class.
+ */
+
+
 public class Message {
 int time;
 String uid,tid,ptid,typeofmessage,text;
