753a60c35c2da1c1ee6af9829d90cfd9a827de16
Alok Kumar <36418056+alokkiitd1729@users.noreply.github.com>
2019-01-26 04:41:18 +0530

    Update MySet.java

--- MySet.java
+++ MySet.java
@@ -1,3 +1,15 @@
+
+
+/**
+ * Project completion Date 18 November 2018
+ *    Author@_Alok_Kumar
+ *    Indian Institute of Technology(IIT) Delhi
+ *
+ *    To Understand the project in enough detail please go to ReadMe file of Project1
+ *     and Visit under the section under the name of this class.
+ */
+
+
 public class MySet<X>  { 
 	public class Node {
 		public X data;
