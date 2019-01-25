42264504b05e62f97f39c6b576e8778615afefcf
Alok Kumar <36418056+alokkiitd1729@users.noreply.github.com>
2019-01-26 04:41:41 +0530

    Update User.java

--- User.java
+++ User.java
@@ -1,3 +1,15 @@
+
+
+/**
+ * Project completion Date 18 November 2018
+ *    Author@_Alok_Kumar
+ *    Indian Institute of Technology(IIT) Delhi
+ *
+ *    To Understand the project in enough detail please go to ReadMe file of Project1
+ *     and Visit under the section under the name of this class.
+ */
+
+
 import java.util.*;
 public class User {
 String uid;	
